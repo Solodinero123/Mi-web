6db194e18a24da08d67f78953fefbaa08c43e19f
Solodinero123 <carmentineo558@gmail.com>
2025-01-18 19:31:40 -0500

    Add files via upload
    Subiendo mi página web

---
+++ script.js
@@ -0,0 +1,3 @@
+ script.jdocument.getElementById("cambiar-color").addEventListener("click", function() {
+    document.body.style.backgroundColor = document.body.style.backgroundColor === "lightblue" ? "#f0f0f0" : "lightblue";
+});
